54a63592530faa63156d0fec1770a453dfdd2a3f
Benjamin Spilker <ben@ne-inc.com>
2025-02-22 23:21:07 -0500

    revised password requirement

--- 2_MakeProxmoxVMs/2A-make-vm-template.sh
+++ 2_MakeProxmoxVMs/2A-make-vm-template.sh
@@ -14,8 +14,6 @@ if [ "$PASSWORD" = "<your-new-password-from-step-1A.3>" ]; then
   echo ""
   echo "Password is still set to the default. Please edit the password using nano 2A-make-vm-template.sh to set a custom password."
   exit 1  # Exit the script with a non-zero status
-else
-  echo "ubuntuprox:$PASSWORD" | chpasswd
 fi
 
 # Also we should set a cloudinit password for our template
