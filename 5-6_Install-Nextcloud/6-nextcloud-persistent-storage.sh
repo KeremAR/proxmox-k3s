083a1667d8878b5f9349423110888e9328b55400
Ben Spilker <bspilker@exactitconsulting.com>
2025-03-23 19:02:34 -0400

    Added additional delay in Script 6

--- 5-6_Install-Nextcloud/6-nextcloud-persistent-storage.sh
+++ 5-6_Install-Nextcloud/6-nextcloud-persistent-storage.sh
@@ -89,10 +89,10 @@ echo "YAML file '$OUTPUT_FILE2' has been created."
 kubectl apply -f nextcloud-temp-pod.yaml
 
 echo ""
-echo "Applying the yaml file and waiting 30 seconds for it to start..."
+echo "Applying the yaml file and waiting 45 seconds for it to start..."
 echo ""
 
-sleep 30
+sleep 45
 
 kubectl get pods -n nextcloud
 
