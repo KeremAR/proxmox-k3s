cb2fc9d359bc9137219f9853e0ad10cd41d5894e
Ben Spilker <ben@ne-inc.com>
2025-03-30 13:08:44 -0400

    added overwrite protocol https to Script 5B

--- 5-6_Install-Nextcloud/5B-install-nextcloud.sh
+++ 5-6_Install-Nextcloud/5B-install-nextcloud.sh
@@ -138,8 +138,9 @@ CONFIG_PATH=\"/var/www/html/config/config.php\" && \
 toppart=\$(head -n 26 \$CONFIG_PATH) && \
 bottompart=\$(tail -n +27 \$CONFIG_PATH) && \
 
-newline=\"   2 => \\\"nextcloud.\$DOMAINNAME\\\"\" && \
-echo \"\$toppart\$newline\$bottompart\" > \$CONFIG_PATH"
+newline=\"'overwriteprotocol' => 'https', \
+newline2=\"   2 => \\\"nextcloud.\$DOMAINNAME\\\"\" && \
+echo \"\$toppart\$newline\$newline2\$bottompart\" > \$CONFIG_PATH"
 
 # Using sed to replace all occurrences of "http://localhost" with "https://nextcloud.$DOMAINNAME"
 
