2bee3a29ee5c9e07396777c8476cc866991b143f
Ben Spilker <ben@ne-inc.com>
2025-04-22 15:34:10 -0400

    corrected script 2D

--- 2_MakeProxmoxVMs/2D-copy-ssh_creds.sh
+++ 2_MakeProxmoxVMs/2D-copy-ssh_creds.sh
@@ -37,16 +37,15 @@ done
 # Step 2D.3: SSH to Admin VM, then download scripts to the admin VM and make them executable
 # The rest of our work for the remainder of the project will be done from here.
 
-# Save the IP of the Admin VM as a txt file in under both ubuntuprox and the admin vm
-
-if [ ! -f ADMIN_VM_IP.txt ]; then
-    echo "$ADMIN_VM_IP" > ADMIN_VM_IP.txt
-    ssh -i ./.ssh/id_rsa ubuntu@$ADMIN_VM_IP "echo $ADMIN_VM_IP > ADMIN_VM_IP.txt"
+# Save the IP of the Admin VM as a txt file under ubuntuprox
+if [ ! -f ADMIN_VM_IP.txt ]; then echo "$ADMIN_VM_IP" > ADMIN_VM_IP.txt 
 fi
 
-# Downloading scripts
-ssh -i ./.ssh/id_rsa ubuntu@$ADMIN_VM_IP << 'EOF'
-  
+# SSH to the Admin VM to get files to it
+ssh -i ./.ssh/id_rsa ubuntu@$ADMIN_VM_IP <<EOF
+
+  if [ ! -f ADMIN_VM_IP.txt ]; then echo "$ADMIN_VM_IP" > ADMIN_VM_IP.txt; fi
+
   # Set permissions for SSH keys
   chmod 600 /home/ubuntu/id_rsa
   chmod 644 /home/ubuntu/id_rsa.pub
@@ -55,21 +54,22 @@ ssh -i ./.ssh/id_rsa ubuntu@$ADMIN_VM_IP << 'EOF'
   echo ""
 
   FILE="3-install-k3s-from-JimsGarage.sh"
-  [ -f "$FILE" ] || curl -sO "https://raw.githubusercontent.com/benspilker/proxmox-k3s/main/3_Install-K3s/$FILE" && chmod +x "$FILE"
+  [ -f "\$FILE" ] || curl -sO "https://raw.githubusercontent.com/benspilker/proxmox-k3s/main/3_Install-K3s/\$FILE" && chmod +x "\$FILE"
 
   FILE="4-install-rancher-ui.sh"
-  [ -f "$FILE" ] || curl -sO "https://raw.githubusercontent.com/benspilker/proxmox-k3s/main/4_RancherInstall/$FILE" && chmod +x "$FILE"
+  [ -f "\$FILE" ] || curl -sO "https://raw.githubusercontent.com/benspilker/proxmox-k3s/main/4_RancherInstall/\$FILE" && chmod +x "\$FILE"
 
   FILE="5A-domainname-dns.sh"
-  [ -f "$FILE" ] || curl -sO "https://raw.githubusercontent.com/benspilker/proxmox-k3s/main/5-6_Install-Nextcloud/$FILE" && chmod +x "$FILE"
+  [ -f "\$FILE" ] || curl -sO "https://raw.githubusercontent.com/benspilker/proxmox-k3s/main/5-6_Install-Nextcloud/\$FILE" && chmod +x "\$FILE"
 
   FILE="5B-optional-test-nextcloud-install.sh"
-  [ -f "$FILE" ] || curl -sO "https://raw.githubusercontent.com/benspilker/proxmox-k3s/main/5-6_Install-Nextcloud/$FILE" && chmod +x "$FILE"
+  [ -f "\$FILE" ] || curl -sO "https://raw.githubusercontent.com/benspilker/proxmox-k3s/main/5-6_Install-Nextcloud/\$FILE" && chmod +x "\$FILE"
 
   FILE="6-nextcloud-mysql-persistent.sh"
-  [ -f "$FILE" ] || curl -sO "https://raw.githubusercontent.com/benspilker/proxmox-k3s/main/5-6_Install-Nextcloud/$FILE" && chmod +x "$FILE"
+  [ -f "\$FILE" ] || curl -sO "https://raw.githubusercontent.com/benspilker/proxmox-k3s/main/5-6_Install-Nextcloud/\$FILE" && chmod +x "\$FILE"
 
 EOF
+
 
 # Step 2D.4: SSH to Admin VM to continue with the next section and execute scripts
 ssh -t -i ./.ssh/id_rsa ubuntu@$ADMIN_VM_IP "ls;bash"
