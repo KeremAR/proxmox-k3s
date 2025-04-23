4299fcef6cc9366f497e69a5b551a9515d8ca5bd
Ben Spilker <ben@ne-inc.com>
2025-04-22 23:38:27 -0400

    Swapped upgrade code block to Script 2D to add in delay

--- 2_MakeProxmoxVMs/2D-copy-ssh_creds.sh
+++ 2_MakeProxmoxVMs/2D-copy-ssh_creds.sh
@@ -1,12 +1,49 @@
 #!/bin/bash
 
-# Step 2D.1: Switch user to ubuntuprox (if you haven't already)
+# Step 2D.0: Switch user to ubuntuprox (if you haven't already)
 
 if [ "$(whoami)" != "ubuntuprox" ]; then
   su - ubuntuprox
 else
   echo "Confirmed user is logged in as ubuntuprox."
 fi
+
+# Step 2D.1 Upgrade all 9 VMs
+
+# Get a list of IP Addresses for all 9 VM's
+
+extract_ip() {
+  local var_name=$1
+  local cidr=$(grep -oP "${var_name}=\"\K[0-9.]+/[0-9]+" 2B-make-vms-from-template.sh)
+  local ip=${cidr%/24}
+  echo "$cidr" "$ip"
+}
+
+read ADMIN_VM_CIDR ADMIN_VM_IP < <(extract_ip "ADMIN_VM_CIDR")
+read TEST_K3S_01_CIDR TEST_K3S_01_IP < <(extract_ip "TEST_K3S_01_CIDR")
+read TEST_K3S_02_CIDR TEST_K3S_02_IP < <(extract_ip "TEST_K3S_02_CIDR")
+read TEST_K3S_03_CIDR TEST_K3S_03_IP < <(extract_ip "TEST_K3S_03_CIDR")
+read TEST_K3S_04_CIDR TEST_K3S_04_IP < <(extract_ip "TEST_K3S_04_CIDR")
+read TEST_K3S_05_CIDR TEST_K3S_05_IP < <(extract_ip "TEST_K3S_05_CIDR")
+read TEST_LONGHORN01_CIDR TEST_LONGHORN01_IP < <(extract_ip "TEST_LONGHORN01_CIDR")
+read TEST_LONGHORN02_CIDR TEST_LONGHORN02_IP < <(extract_ip "TEST_LONGHORN02_CIDR")
+read TEST_LONGHORN03_CIDR TEST_LONGHORN03_IP < <(extract_ip "TEST_LONGHORN03_CIDR")
+
+# Run a for each loop to apply updates to all VM's
+
+for vm_ip in $ADMIN_VM_IP $TEST_K3S_01_IP $TEST_K3S_02_IP $TEST_K3S_03_IP $TEST_K3S_04_IP $TEST_K3S_05_IP $TEST_LONGHORN01_IP $TEST_LONGHORN02_IP $TEST_LONGHORN03_IP; do
+    while true; do
+        if ping -c 1 "$vm_ip" &> /dev/null; then
+            echo "$vm_ip is up."
+            ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -t -i ./.ssh/id_rsa ubuntu@"$vm_ip" \
+            "sudo apt update && sudo apt full-upgrade -y [ -f /var/run/reboot-required ] && sudo reboot"
+            break
+        else
+            echo "$vm_ip is not responding, retrying..."
+            sleep 2
+        fi
+    done
+done
 
 # Step 2D.2: Ping the admin machine. Once reachable, copy SSH creds to it to be used for other VMs.
 
